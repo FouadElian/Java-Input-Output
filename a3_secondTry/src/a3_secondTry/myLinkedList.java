0e608720704d2c491f3789f3b3f9df1a80c47c4f
fouad elian <157070771+FouadElian@users.noreply.github.com>
2024-05-07 17:48:26 -0400

    Update myLinkedList.java

--- a3_secondTry/src/a3_secondTry/myLinkedList.java
+++ a3_secondTry/src/a3_secondTry/myLinkedList.java
@@ -1,9 +1,5 @@
 package a3_secondTry;
 
-//-----------------------------------
-// Assignment 3
-// Written by: Fouad Elian 40273045
-//-----------------------------------
 
 class NoSuchElement extends Exception{
 	public NoSuchElement(String b) {
